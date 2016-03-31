afb714e810113a1ff92c4cdd868b19300e06fbd9
yfl0925 <1129507011@qq.com>
2016-03-31 09:52:52 +0800

    add func and admin class

--- mypc/libs/functions/global.func.php
+++ mypc/libs/functions/global.func.php
@@ -268,3 +268,17 @@ function ip(){
 
 	return preg_match('/[\d\.]{7,15}/', $ip, $matches) ? $matches[0] : '';
 }
+
+
+
+
+/**
+* 获取当前页面完整URL地址
+*/
+function get_url(){
+	$sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '433' ? 'https://' : 'http://';
+	$php_self = $_SERVER['PHP_SELF'] ? safe_replace($_SERVER['PHP_SELF']) : safe_replace($_SERVER['SCRIPT_NAME']);
+	$path_info = isset($_SERVER['PATH_INFO']) ? safe_replace($_SERVER['PATH_INFO']) : '';
+	$relate_url = isset($_SERVER['REQUEST_URI']) ? safe_replace($_SERVER['REQUEST_URI']) : $php_self . (isset($_SERVER['QUERY_STRING']) ? '?' . safe_replace($_SERVER['QUERY_STRING']) : $path_info);
+	return $sys_protocal . (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '') . $relate_url;
+}
---
+++ mypc/modules/admin/classes/admin.class.php
@@ -0,0 +1,9 @@
+<?php
+defined('IN_MYPC') or exit('No permission resources.');
+mp_base::load_sys_class('session_mysql');  //数据库session存储模型
+
+//定义后台标识
+class admin{
+  
+}
+ ?>
